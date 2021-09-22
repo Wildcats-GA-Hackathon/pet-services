155b230286c34ab2c136aaa3f7de7281c77c7891
Michael Grebowicz <maciejgrebowicz@gmail.com>
2021-09-21 22:09:32 -0700

    code NavBar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import NavBar from './components/NavBar/NavBar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Container } from '@material-ui/core';
+import Home from './components/Home/Home';
 
 const App = () => {
   return (
-    <div>
-      <h1>Pet Services</h1>
-    </div>
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <NavBar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/auth' exact component={Auth} />
+        </Switch>
+        <Home />
+      </Container>
+    </BrowserRouter>
   )
 }
 
-export default App;
+export default App; 
